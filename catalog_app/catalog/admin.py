42ff06ba3b3ee705e990fbbca117441f683bf02c
Alexandr <alexandr.panteleev2000@gmail.com>
2024-03-22 17:56:08 +0300

    Removed unused imports

--- catalog_app/catalog/admin.py
+++ catalog_app/catalog/admin.py
@@ -1,7 +1,5 @@
 from typing import Any
 from django.contrib import admin
-from django.db.models.query import QuerySet
-from django.http import HttpRequest
 
 from .models import Book, Category
 
--- catalog_app/users/views.py
+++ catalog_app/users/views.py
@@ -1,7 +1,6 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView
 
-
 from .serializers import CreateUserSerializer
 
 
